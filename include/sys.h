5579f51dff2f84fcd9ffb70e346ea69ae8cd1633
José Ignacio Alamos Aste <jialamos@uc.cl>
2016-03-21 01:47:58 +0100

    Finished vital linked list functions

--- include/sys.h
+++ include/sys.h
@@ -1,19 +1,20 @@
 #ifndef SYS_H
 #define SYS_H
+#include <stdlib.h>
 
 #define LINKED_LIST_SIZE (255)
+
 
 typedef struct ll_node_t
 {
 	void *data;
-	ll_node_t *next;
+	struct ll_node_t *next;
 } ll_node_t;
 
 typedef struct linked_list_t
 {
-	ll_node_t *tail:		
-	ll_node_t *head;
-
+	ll_node_t *tail;		
+	ll_node_t *head;		
 } linked_list_t;
 
 static inline void linked_list_init(linked_list_t *linked_list)
@@ -25,14 +26,14 @@ static inline void linked_list_add(linked_list_t *linked_list, void *data)
 {
 	if(linked_list->tail == NULL)
 	{
-		linked_list->head = (ll_node_t)* malloc(sizeof(ll_node_t));
+		linked_list->head = (ll_node_t*) malloc(sizeof(ll_node_t));
 		linked_list->head->next = NULL;
 		linked_list->head->data = data;
 		linked_list->tail = linked_list->head;
 	}
 	else
 	{
-		linked_list->tail->next = (ll_node_t)* malloc(sizeof(ll_node_t));
+		linked_list->tail->next = (ll_node_t*) malloc(sizeof(ll_node_t));
 		linked_list->tail = linked_list->tail->next;
 		linked_list->tail->next = NULL;
 		linked_list->tail->data = data;
@@ -40,7 +41,7 @@ static inline void linked_list_add(linked_list_t *linked_list, void *data)
 }
 //static inline void linked_list_remove_element(linked_list_t linked_list, void *data);
 //static inline void *linked_list_pop(linked_list_t linked_list);
-static inline int linked_list_count_elem(linked_list_t linked_list)
+static inline int linked_list_count_elem(linked_list_t *linked_list)
 {
 	ll_node_t *ptr;
 	ptr = linked_list->head;
